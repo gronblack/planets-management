b6c0c65b2c7deca3b8f77585dad54b00ae1b137c
gronblack <30817266+gronblack@users.noreply.github.com>
2021-11-03 13:16:23 +0700

    init

---
+++ src/main/java/com/github/gronblack/pm/PlanetsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.gronblack.pm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetsManagementApplication.class, args);
+	}
+
+}
