b3a2d1ee5ecc9746cb58ce6341cc951262af477b
gronblack <30817266+gronblack@users.noreply.github.com>
2021-11-15 15:22:37 +0700

    refactoring

--- src/main/java/com/github/gronblack/pm/repository/PlanetRepository.java
+++ src/main/java/com/github/gronblack/pm/repository/PlanetRepository.java
@@ -2,7 +2,6 @@ package com.github.gronblack.pm.repository;
 
 import com.github.gronblack.pm.model.Planet;
 import org.springframework.data.jpa.repository.EntityGraph;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -12,8 +11,4 @@ public interface PlanetRepository extends BaseRepository<Planet> {
     @EntityGraph(attributePaths = {"lord"}, type = EntityGraph.EntityGraphType.LOAD)
     @Query("SELECT p FROM Planet p WHERE p.id=:id")
     Optional<Planet> getWithLord(int id);
-
-    @Modifying
-    @Query(value = "UPDATE Planet p SET p.lord.id =:lordId WHERE p.id=:id")
-    void setLord(int id, int lordId);
 }
